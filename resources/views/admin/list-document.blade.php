d9e7837801e5b6d89e3ecb377dc98ccad799cf5b
amun1303 <pigbanguy@gmail.com>
2021-08-16 00:03:16 +0700

    list document 40%

--- resources/views/admin/list-document.blade.php
+++ resources/views/admin/list-document.blade.php
@@ -13,25 +13,25 @@ use App\Models\DocumentType;
             <th class="column-title">Tên tài liệu</th>
             <th class="column-link" width=" 300px">Link</th>
             <th class="column-type_id">Loại</th>
-            <th class="column-admin_id">Người tạo</th>
+            {{-- <th class="column-admin_id">Người tạo</th> --}}
         </tr>
     </thead>
     <tbody>
         @foreach ($doc_list as $row)
             <tr data-key="1">
-                <td class="column-title">
+                <td class="column-title" width="40%">
                     {{ $row->title }}
                 </td>
-                <td class="column-link" width=" 300px">
+                <td class="column-link" width="40%">
                     <a href="{{ $row->link }}" target="_blank"
                         style="overflow-wrap: anywhere;">{{  $row->link }}</a>
                 </td>
                 <td class="column-type_id">
                     <span class="label label-danger">{{ DocumentType::find($row->type_id)->title }}</span>
                 </td>
-                <td class="column-admin_id">
+                {{-- <td class="column-admin_id">
                     {{ Administrator::find($row->admin_id)->name }}
-                </td>
+                </td> --}}
             </tr>
         @endforeach
     </tbody>
