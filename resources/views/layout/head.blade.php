2b0dcad625f18583af35b4792e34373833e6fc9b
amun1303 <pigbanguy@gmail.com>
2021-09-14 21:27:00 +0700

    add GA

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,17 +1,34 @@
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Thần Số Học - Map For Success</title>
-	<meta name="description" content="A free responsive website template made exclusively for Frittt by Themesforce and Sarfraz Shaukat">
-	<meta name="keywords" content="website template, css3, one page, bootstrap, app template, web app, start-up">
-	<meta name="author" content="Themesforce and Sarfraz Shaukat for Frittt">
-	<link rel="icon" type="image/png" href="{{ env('APP_URL') }}/vendor/free-css-sports/favicons/favicon-16x16.png" sizes="16x16">
-	<link rel="stylesheet" href="{{ env('APP_URL') }}/vendor/free-css-sports/css/bootstrap.css">
-	<link rel="stylesheet" href="{{ env('APP_URL') }}/vendor/free-css-sports/fonts/font-awesome-4.3.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="{{ env('APP_URL') }}/css/floating-hotline-with-number.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thần Số Học - Map For Success</title>
+    <meta name="description"
+        content="A free responsive website template made exclusively for Frittt by Themesforce and Sarfraz Shaukat">
+    <meta name="keywords" content="website template, css3, one page, bootstrap, app template, web app, start-up">
+    <meta name="author" content="Themesforce and Sarfraz Shaukat for Frittt">
+    <link rel="icon" type="image/png" href="{{ env('APP_URL') }}/vendor/free-css-sports/favicons/favicon-16x16.png"
+        sizes="16x16">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/vendor/free-css-sports/css/bootstrap.css">
+    <link rel="stylesheet"
+        href="{{ env('APP_URL') }}/vendor/free-css-sports/fonts/font-awesome-4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/css/floating-hotline-with-number.css">
 
     <link rel="stylesheet" href="{{ env('APP_URL') }}/vendor/free-css-sports/css/all.css?v=20210805">
     <link rel="stylesheet" href="{{ env('APP_URL') }}/css/style.css?v=20210805">
 
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Source+Sans+Pro:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Source+Sans+Pro:400,700,400italic,700italic'
+        rel='stylesheet' type='text/css'>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q97MK9PP27"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-Q97MK9PP27');
+    </script>
 </head>
