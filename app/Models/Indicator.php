a07f07575ecab88539a731d2ff52752c1d5d8bcd
amun1303 <pigbanguy@gmail.com>
2022-03-02 15:37:55 +0700

    Lỗi tính chỉ số Linh Hồn

--- app/Models/Indicator.php
+++ app/Models/Indicator.php
@@ -14,8 +14,7 @@ class Indicator extends Model
     {
         $r = 0;
         $s = intval($s);
-
-        if ($s < 9 || $s == 11 || $s == 22 || $s == 33) {
+        if ($s < 10 || $s == 11 || $s == 22 || $s == 33) {
             return $s;
         } else {
             foreach (str_split($s) as $char) {
@@ -52,16 +51,16 @@ class Indicator extends Model
 
     static function textToNumber($s)
     {
-        $one = ['a', 'j', 's', 'A', 'J', 'S'];
-        $two = ['b', 'k', 't', 'B', 'K', 'T'];
-        $three = ['c', 'l', 'u', 'C', 'L', 'U'];
-        $four = ['d', 'm', 'v', 'D', 'M', 'V'];
-        $five = ['e', 'n', 'w', 'E', 'N', 'W'];
-        $six = ['f', 'o', 'x', 'F', 'O', 'X'];
-        $seven = ['g', 'p', 'y', 'G', 'P', 'Y'];
-        $eight = ['h', 'q', 'z', 'H', 'Q', 'Z'];
-        $nine = ['i', 'r', 'I', 'R'];
-        $str = '';
+        $one    = ['a', 'j', 's', 'A', 'J', 'S'];
+        $two    = ['b', 'k', 't', 'B', 'K', 'T'];
+        $three  = ['c', 'l', 'u', 'C', 'L', 'U'];
+        $four   = ['d', 'm', 'v', 'D', 'M', 'V'];
+        $five   = ['e', 'n', 'w', 'E', 'N', 'W'];
+        $six    = ['f', 'o', 'x', 'F', 'O', 'X'];
+        $seven  = ['g', 'p', 'y', 'G', 'P', 'Y'];
+        $eight  = ['h', 'q', 'z', 'H', 'Q', 'Z'];
+        $nine   = ['i', 'r', 'I', 'R'];
+        $str    = '';
         foreach (str_split($s) as $char) {
             switch ($char) {
                 case (in_array($char, $one)):
@@ -98,20 +97,20 @@ class Indicator extends Model
 
     static function convertViToEn($str)
     {
-        $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", "a", $str);
-        $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", "e", $str);
-        $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", "i", $str);
-        $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", "o", $str);
-        $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", "u", $str);
-        $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", "y", $str);
-        $str = preg_replace("/(đ)/", "d", $str);
-        $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", "A", $str);
-        $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", "E", $str);
-        $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", "I", $str);
-        $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", "O", $str);
-        $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", "U", $str);
-        $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", "Y", $str);
-        $str = preg_replace("/(Đ)/", "D", $str);
+        $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/",    "a", $str);
+        $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/",                "e", $str);
+        $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/",                            "i", $str);
+        $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/",    "o", $str);
+        $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/",                "u", $str);
+        $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/",                            "y", $str);
+        $str = preg_replace("/(đ)/",                                    "d", $str);
+        $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/",    "A", $str);
+        $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/",                "E", $str);
+        $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/",                            "I", $str);
+        $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/",    "O", $str);
+        $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/",                "U", $str);
+        $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/",                            "Y", $str);
+        $str = preg_replace("/(Đ)/",                                    "D", $str);
         //$str = str_replace(" ", "-", str_replace("&*#39;","",$str));
         return $str;
     }
@@ -125,9 +124,9 @@ class Indicator extends Model
         ];
 
         /// Nếu chuoi có 1 ký tự và là chữ Y thì xác định là nguyên âm luôn
-        if (strlen($str)==1 && ($str == 'y' || $str == 'Y')) { 
+        if (strlen($str) == 1 && ($str == 'y' || $str == 'Y')) {
             $s['vowel'] .= $str;
-        /// ngược lại nếu len > 1 và ko phải Y thì kiểm tra tiếp
+            /// ngược lại nếu len > 1 và ko phải Y thì kiểm tra tiếp
         } else {
             foreach (str_split($str) as $index => $c) {
                 if (in_array($c, $vowels)) {
@@ -150,7 +149,7 @@ class Indicator extends Model
             }
         }
 
-        
+
         return $s;
     }
 
@@ -194,16 +193,11 @@ class Indicator extends Model
         return $total;
     }
 
-    public static function HeartDesireCalc($customer) 
+    public static function HeartDesireCalc($customer)
     {
         $fn = self::convertViToEn($customer->last_name) . ' ' . self::convertViToEn($customer->first_name);
-        $total_ln = 0;
-        $fnArr = explode(' ', $fn);
-        foreach ($fnArr as $index => $str) {
-            $vowels = self::getVowelAndConsonant($str)['vowel'];
-            $total_ln += self::total(self::textToNumber($vowels));
-        }
-        return self::total($total_ln);
+        $vowels = self::getVowelAndConsonant($fn)['vowel'];
+        return self::total(self::textToNumber($vowels));
     }
     public static function PersonalityCalc($customer)
     {
