77d06ad5a41821bb5dbf5d3fb5ad8e7ca60253c0
amun1303 <pigbanguy@gmail.com>
2023-11-08 09:26:10 +0200

    filter

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use Encore\Admin\Auth\Database\Role;
 use Encore\Admin\Controllers\UserController as EncoreUserController;
 
 class UserController extends EncoreUserController
@@ -9,6 +10,8 @@ class UserController extends EncoreUserController
     protected function grid()
     {
         $grid = parent::grid();
+
+        $grid->model()->orderBy('id', 'desc');
         $grid->column('parent_id', 'Tuyến trên');
 
         $grid->filter(function ($filter) {
@@ -17,6 +20,14 @@ class UserController extends EncoreUserController
             // Add a column filter
             $filter->like('name', 'Tìm theo Tên');
             $filter->like('username', 'Tìm theo Username');
+
+            // // Sử dụng callback để thêm filter theo roles
+            // $filter->where(function ($query) use ($filter) {
+            //     // Lọc dựa trên tên của vai trò từ bảng roles
+            //     $query->whereHas('roles', function ($q) {
+            //         $q->where('name', 'equal', '%{$this->input}%');
+            //     });
+            // }, 'Vai trò')->select(Role::all()->pluck('id')->label('name'));
         });
 
 
